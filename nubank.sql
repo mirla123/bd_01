53bf0bed1926e243872225747fc6b7567f0253a7
mirla123 <mirlasilva202099@gmail.com>
2022-03-14 22:27:30 -0300

    create banco de dados

---
+++ nubank.sql
@@ -0,0 +1,108 @@
+-- MariaDB dump 10.19  Distrib 10.4.22-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: nubank
+-- ------------------------------------------------------
+-- Server version	10.4.22-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `funcionarios`
+--
+
+DROP TABLE IF EXISTS `funcionarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `funcionarios` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `telefone` varchar(10) DEFAULT NULL,
+  `cargo` varchar(20) DEFAULT NULL,
+  `salário` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `funcionarios`
+--
+
+LOCK TABLES `funcionarios` WRITE;
+/*!40000 ALTER TABLE `funcionarios` DISABLE KEYS */;
+INSERT INTO `funcionarios` VALUES (1,'Lucas Neto','12345678914','981016563','entreagdor','1122');
+/*!40000 ALTER TABLE `funcionarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa_fisica`
+--
+
+DROP TABLE IF EXISTS `pessoa_fisica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_fisica` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `ag` varchar(10) DEFAULT NULL,
+  `conta` varchar(10) DEFAULT NULL,
+  `pix` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_fisica`
+--
+
+LOCK TABLES `pessoa_fisica` WRITE;
+/*!40000 ALTER TABLE `pessoa_fisica` DISABLE KEYS */;
+INSERT INTO `pessoa_fisica` VALUES (1,'Pedro Alvares Cabral','12312312332','123','123','pedro@portugal.com'),(2,'Ana Maria Lima','23136525423','245','456','anamaria@.com'),(3,'Jose Lopes','45612378936','123','564','9781234561'),(4,'Maria Silva','14725836956','456','369','mariasilva@.com'),(5,'Luan Santana','45612378932','589','632','Luansantana@.com');
+/*!40000 ALTER TABLE `pessoa_fisica` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pessoa_juridica`
+--
+
+DROP TABLE IF EXISTS `pessoa_juridica`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pessoa_juridica` (
+  `id` int(11) DEFAULT NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `cpf` varchar(11) DEFAULT NULL,
+  `ag` varchar(10) DEFAULT NULL,
+  `conta` varchar(10) DEFAULT NULL,
+  `pix` varchar(40) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pessoa_juridica`
+--
+
+LOCK TABLES `pessoa_juridica` WRITE;
+/*!40000 ALTER TABLE `pessoa_juridica` DISABLE KEYS */;
+INSERT INTO `pessoa_juridica` VALUES (1,'João Souza','25636965458','254','589','joaosouza@.com'),(2,'Carlos Silva','14725836912','456','698','carlossilva@.com'),(2,'Carlos Silva','14725836912','456','698','carlossilva@.com'),(4,'Cristina Ribeiro','147258369','123','789','78945612'),(5,'Lucas Veiga','45678912345','4562','4565','457961351');
+/*!40000 ALTER TABLE `pessoa_juridica` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-14 22:19:31
